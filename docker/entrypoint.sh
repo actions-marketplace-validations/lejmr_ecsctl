4a63055d9d656dd4c5fd710519c134b533db8314
Miloš Kozák <milos.kozak@lejmr.com>
2021-02-15 22:28:20 +0100

    Fix name of varible

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
+# Set to fail asap
+set -e pipetail
+
 # Defines output 
 export OUTPUT_TASK_DEFINITION_PATH="task-definition-rendered.json"
 export INPUT_TASK_DEFINITION="$(env | sed -n 's/^INPUT_TASK-DEFINITION=\(.*\)/\1/p')"
-env
-
-# Set to fail asap
-set -e pipetail
+env 
 
 # Base command
 CMD="ecs-render --td ${INPUT_TASK_DEFINITION} --val $INPUT_VALUES"
@@ -17,7 +17,6 @@ if [ -n "$INPUT_IMAGE" ]; then
 fi
 
 # Execute the command
-echo $CMD
 sh -c "$CMD" > $OUTPUT_TASK_DEFINITION_PATH
 
 # Final return of path to the file
