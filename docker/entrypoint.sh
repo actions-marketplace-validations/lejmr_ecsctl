67533aa17256de3cf685318c3830f697a2d9a75e
Milos Kozak <mkozak@finmason.com>
2021-04-20 20:03:47 +0200

    Adds support for Jenkins

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,22 +1,26 @@
 #!/bin/sh
 
 # Set to fail asap
-set -e pipetail
+set -e 
 
 # Defines output 
 export OUTPUT_TASK_DEFINITION_PATH="task-definition-rendered.json"
 export INPUT_TASK_DEFINITION="$(env | sed -n 's/^INPUT_TASK-DEFINITION=\(.*\)/\1/p')"
 
-# Base command
-CMD="ecs-render --td ${INPUT_TASK_DEFINITION} --val $INPUT_VALUES"
+if [ -z "$OUTPUT_TASK_DEFINITION_PATH" ] && [ -z "$INPUT_TASK_DEFINITION" ]; then
+    # Base command
+    CMD="ecs-render --td ${INPUT_TASK_DEFINITION} --val $INPUT_VALUES"
 
-# Insert override
-if [ -n "$INPUT_IMAGE" ]; then
-    CMD="${CMD} --set image=$INPUT_IMAGE"
+    # Insert override
+    if [ -n "$INPUT_IMAGE" ]; then
+        CMD="${CMD} --set image=$INPUT_IMAGE"
+    fi
+
+    # Execute the command
+    sh -c "$CMD" > $OUTPUT_TASK_DEFINITION_PATH
+
+    # Final return of path to the file
+    echo "::set-output name=task-definition::$OUTPUT_TASK_DEFINITION_PATH"
+else
+    exec $@
 fi
-
-# Execute the command
-sh -c "$CMD" > $OUTPUT_TASK_DEFINITION_PATH
-
-# Final return of path to the file
-echo "::set-output name=task-definition::$OUTPUT_TASK_DEFINITION_PATH"
