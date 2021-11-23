d8514f63a7eb2473486a0b27a05c3c11d6998656
Milos Kozak <milos.kozak@lejmr.com>
2021-11-23 13:56:49 +0100

    Update build process

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ecscontrol',
-    version='0.1.0',
+    version='1.0.0',
     license='MIT',
     description = 'Tool for automation of deployments to AWS ECS',
     author = 'Milos Kozak',
